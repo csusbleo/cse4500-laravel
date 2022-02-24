e9c989a091bae9705c56864a5a356473f648529f
leoli <yli@csusb.edu>
2022-02-24 14:28:12 -0800

    Update db_test.blade.php

--- resources/views/db_test.blade.php
+++ resources/views/db_test.blade.php
@@ -3,11 +3,6 @@
     try {
         \DB::connection()->getPDO();
         echo \DB::connection()->getDatabaseName();
-
-        $tables = \DB::select('SHOW TABLES');
-        foreach ($tables as $table) {
-            echo $table->Tables_in_d4q3oni2d3ftgh;
-        }
     } catch (\Exception $e) {
           echo 'None';
     }
