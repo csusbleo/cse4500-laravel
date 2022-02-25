b8799b98e9e4eb0b59b978a14ee946eae389701e
leoli <yli@csusb.edu>
2022-02-24 16:23:45 -0800

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,17 +3,13 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Users</h1>
 @stop
 
 @section('content')
     <p>
       {{ print_r($users) }}
     </p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
