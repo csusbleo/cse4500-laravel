58b82e0cf4ed37baf58087842c08667758d53861
leoli <yli@csusb.edu>
2022-02-25 10:19:41 -0800

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -249,7 +249,7 @@ return [
         [
             'text' => 'To Do \'s',
             'url'  => 'todos',
-            'icon' => 'far fa-fw fa-list-check'
+            'icon' => 'far fa-fw fa-clipboard-list-check'
         ],
         [
             'text' => 'Schedule',
