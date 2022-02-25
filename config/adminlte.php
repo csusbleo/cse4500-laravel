c4d22936d8c04d936970e3f76ca9581ec7159de0
leoli <yli@csusb.edu>
2022-02-25 13:12:43 -0800

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -369,6 +369,21 @@ return [
                 ],
             ],
         ],
+        'Fullcalendar' => [
+        'active' => true,
+        'files' => [
+            [
+                'type' => 'js',
+                'asset' => false,
+                'location' => '//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js',
+            ],
+            [
+                'type' => 'css',
+                'asset' => true,
+                'location' => '//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.css',
+            ],
+        ],
+    ],
     ],
 
     /*
