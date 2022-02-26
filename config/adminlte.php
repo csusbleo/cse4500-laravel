a38b272c10edcc3b2c9a42ca4e6e34da022c9dd9
farmerleo <35739392+liyou222@users.noreply.github.com>
2022-02-25 20:48:20 -0800

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -300,7 +300,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
@@ -369,7 +369,7 @@ return [
                 ],
             ],
         ],
-  
+
     ],
 
     /*
