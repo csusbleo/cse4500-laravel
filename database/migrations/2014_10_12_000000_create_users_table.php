70301d82efd36df2588301bfbe282ca0801ec42c
Leo Li <98563019+csusbleo@users.noreply.github.com>
2022-02-28 22:27:54 -0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,22 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('users')) {
-            // The "users" table does not exist...
-            Schema::create('users', function (Blueprint $table) {
-                $table->id();
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
-                $table->string('password');
-                $table->rememberToken();
-                $table->timestamps();
-            });
-        } else {
-            Schema::table('users', function (Blueprint $table) {
-                $table->string('role');
-            });
-        }
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
