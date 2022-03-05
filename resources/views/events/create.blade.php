d26e28638d521e563fde4862215717ff0d1f560a
farmerleo <35739392+liyou222@users.noreply.github.com>
2022-03-04 20:25:48 -0800

    Update create.blade.php

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -10,8 +10,8 @@
 <form method="post" action="{{ route('calendar.store') }}" >
     @csrf
     <x-adminlte-input name="title" label="Title" />
-    <x-adminlte-input-date name="start_at"  :config="['format' => 'DD/MM/YYYY HH:mm']" label="Start" />
-    <x-adminlte-input-date name="end_at"  :config="['format' => 'DD/MM/YYYY HH:mm']" label="End" />
+    <x-adminlte-input name="start_at" type="date" label="Start" />
+    <x-adminlte-input name="end_at"  type="date" label="End" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
