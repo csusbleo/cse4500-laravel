4146efd7ebf0eb8b50f63b664d48b36a742ea028
Leo Li <98563019+csusbleo@users.noreply.github.com>
2022-04-04 11:12:34 -0700

    Update SampleController.php

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -1,16 +1,15 @@
+<?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Calendar;
+use App/Models/Calendar;
 
 class CalendarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $events = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
     }
+    
+}
